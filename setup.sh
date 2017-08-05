b94cd299e71e805aea119a381bade74f010f21a3
Joel Rodriguez <joelerll@gmail.com>
2017-08-05 00:02:49 -0500

    inicial

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# instalar dependencias necesarias script de clonar las librerias que se necesitan, compilarlas
+
+## dependencias , git, wget o curl
+
+
+## dependencias de proyecto: jsmn, libmicrohttpd, mtab, libudev, libusb
+
+## dependencias de cliente: python3, virtualenv, instalar los requirements .txt
+
+## colocar el virtualenv en una carpeta llamada virtualenv/
